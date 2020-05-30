8f6fa7a3fca09cc9626160c15465de9ab25e12ef
sudorr <43164018+suyeonkimm@users.noreply.github.com>
2020-05-31 07:15:23 +0900

    2020_05_31 led 및 앰프 연결
    wemos d1에 led와 앰프 연결 작업 진행.

---
+++ IoT_department/speaker.ino
@@ -0,0 +1,53 @@
+int speaker = 2; //wemos d1의 tx1/d9 핀에 해당
+
+void setup() {
+
+pinMode(speaker, OUTPUT);
+
+}
+
+//스쿨존에 적용되는 프로젝트인 만큼 경고음은 학교 종소리로 구현
+void loop() {
+
+tone(speaker, 440);
+delay(700);
+noTone(speaker);
+delay(300);//라
+
+tone(speaker, 349);
+delay(700);
+noTone(speaker);
+delay(300);//파
+
+tone(speaker, 392);
+delay(700);
+noTone(speaker);
+delay(300);//솔
+
+tone(speaker, 262);
+delay(700);
+noTone(speaker);
+delay(300);//도
+
+tone(speaker, 262);
+delay(700);
+noTone(speaker);
+delay(300);//도
+
+tone(speaker, 392);
+delay(700);
+noTone(speaker);
+delay(300);//솔
+
+tone(speaker, 440);
+delay(700);
+noTone(speaker);
+delay(300);//라
+
+tone(speaker, 349);
+delay(700);
+noTone(speaker);
+delay(300);//파
+
+
+}
