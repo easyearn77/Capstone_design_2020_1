35a928e1eee4dd56dcc9117a02d7021b26eacd2e
32152665 <gordnflwlq@naver.com>
2020-06-02 19:16:11 +0900

    gps 서버로 전달
    gps를 서버로 전달하게끔 수정

--- app 개발 파트/SoketioEX/app/src/main/java/com/example/soketioex/MainActivity.java
+++ app 개발 파트/SoketioEX/app/src/main/java/com/example/soketioex/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.soketioex;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.location.LocationListener;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
@@ -19,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     EditText editText;
     TextView textView;
+    TextView txtResult;
 
 
 
@@ -28,8 +36,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtResult = (TextView)findViewById(R.id.txtResult);
+
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if ( Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( MainActivity.this, new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },
+                    0 );
+        }
+        else{
+            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            String provider = location.getProvider();
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            double altitude = location.getAltitude();
+
+            txtResult.setText("위치정보 : " + provider + "\n" +
+                    "위도 : " + longitude + "\n" +
+                    "경도 : " + latitude + "\n" +
+                    "고도  : " + altitude);
+
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 1초마다 자신의 gps를 갱신
+                    1000,
+                    1,
+                    gpsLocationListener);
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    1000,
+                    1,
+                    gpsLocationListener);
+
+
+
+
+        }
+
+
+
         try {
-            socket = IO.socket("http://172.30.1.20:3000");
+            socket = IO.socket("http://172.30.1.60:8080"); // http:// + 현재 와이파이 ip + :8080
         }catch (Exception e) {
             e.printStackTrace();
         }
@@ -40,12 +85,18 @@ public class MainActivity extends AppCompatActivity {
                 socket.emit("message_from_client", "Hi~ 나는 안드로이드야.");
             }
         }).on("message_from_server", new Emitter.Listener() {
+
+
             @Override
             public void call(final Object... args) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         textView.setText(args[0].toString());
+                        String msg1 = txtResult.getText().toString();
+                        socket.emit("message_from_client", msg1);
+                        // 이 부분에 서버가 앱에서 받은 gps와 아두이노 에서 얻은 gps의 거리를 계산하여 그 거리가 100m 이내였다면 알림이 왔을것이고 toast메세지, 소리 등을 띄워주면될듯.
+
                     }
                 });
             }
@@ -53,8 +104,10 @@ public class MainActivity extends AppCompatActivity {
 
         socket.connect();
 
+
         editText = (EditText) findViewById(R.id.editText);
         textView = (TextView) findViewById(R.id.textView);
+
         button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -65,4 +118,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    final LocationListener gpsLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+
+            String provider = location.getProvider();
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            double altitude = location.getAltitude();
+
+            txtResult.setText("위치정보 : " + provider + "\n" + // 여기 setText에서 1초마다 txtResult변수에 새로운 값이 들어가는중
+                    "위도 : " + longitude + "\n" +
+                    "경도 : " + latitude + "\n" +
+                    "고도  : " + altitude);
+
+            // 그렇다면 서버에서 gps요청이 올때마다 자신의 달라진 gps값을 송신할수 있음.
+
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+    };
 }
--- app 개발 파트/nodejs/helloworld.js
+++ app 개발 파트/nodejs/helloworld.js
@@ -12,8 +12,10 @@ http.listen(8080, function(){
 
 
 io.sockets.on('connection', function (socket){
+
    //원격에서 접속이 되면 기본 응답
-   socket.emit('message_from_server', 'hello, world');
+   // 아두이노 부분 예상. if 아두이노 센서로부터 신호가 들어왔을 때
+   socket.emit('message_from_server', '현재 gps 값을 요청합니다.');
 
   //메세지가 들어 오면 응답
    socket.on('message_from_client', function (msg){
@@ -21,8 +23,16 @@ io.sockets.on('connection', function (socket){
      /*받은 메세지를 되돌려 주자.
      아니면 받은 데이터를 이용 라즈베리파에서 뭐든 할 수 있다.
      */
-     socket.emit('message_from_server', '"' +msg+ '" 서비스에 연결되었습니다.');
+
+
+     // 이 부분에서 앱에서 받은 gps와 아두이노 에서 얻은 gps의 거리를 계산
+     // 만약 그 거리가 100m 이내라면 믿의 코드를 활성화시켜 알림 전
+     // socket.emit('message_from_server', '위험 알림을 활성화합니다');
+     // socket.emit('message_from_server', '"' +msg+ '" gps 수신완료');
    });
+
+
+
 
 
 });
