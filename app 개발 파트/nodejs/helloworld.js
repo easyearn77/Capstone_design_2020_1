435c99f5d3ac824d9f1e7af6820b0a8338ae234b
32152665 <gordnflwlq@naver.com>
2020-06-10 13:02:46 +0900

    서버 js 필요부분
    서버에서 js 필요한 부분

--- app 개발 파트/nodejs/helloworld.js
+++ app 개발 파트/nodejs/helloworld.js
@@ -25,10 +25,31 @@ io.sockets.on('connection', function (socket){
      */
 
 
-     // 이 부분에서 앱에서 받은 gps와 아두이노 에서 얻은 gps의 거리를 계산
-     // 만약 그 거리가 100m 이내라면 믿의 코드를 활성화시켜 알림 전
+// 이 부분에서 앱에서 받은 gps와 아두이노 에서 얻은 gps의 거리를 계산
+// 만약 그 거리가 100m 이내라면 믿의 코드를 활성화시켜 알림 전송
+
+
+  /*   function getDistanceFromLatLonInKm(lat1,lng1,lat2,lng2) {
+    function deg2rad(deg) {
+        return deg * (Math.PI/180)
+    }
+
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lng2-lng1);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+} */
+
+
+// 이건 km 단위니까 0.01 * d 가 100 이내면 전송하게끔 if문 식으로 만들어야  함.
+// 그래서 socket.emit 식으로 보내면 될듯.
+
      // socket.emit('message_from_server', '위험 알림을 활성화합니다');
      // socket.emit('message_from_server', '"' +msg+ '" gps 수신완료');
+     // 이 경우, emit 리스너를 하나 더 만드는게 나을듯. message_from_server 말고 하나 더 만들어서 알림만 띄우도록.
    });
 
 
